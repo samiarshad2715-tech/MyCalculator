e761b5dec1ea3e42515efd3e5505385fd2ef289a
Sami Arshad <samiarshad2715@gmail.com>
2026-05-29 21:08:52 +0530

    Update script.js
    Updated js! Added AC and DEL funtionality

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
 const button = document.querySelectorAll("button");
 const display = document.querySelector(".display");
-const operator = document.querySelectorAll(".opeartor");
+const operator = document.querySelectorAll(".operator");
 const displaybar = [];
 
 
-button.forEach((btn)=>{
-     let value = btn.innerText;
+button.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        let value = btn.innerText;
 
-    btn.addEventListener("click",()=>{
-
-        if (display.innerText==="0"){
-            display.innerText=value;
+        if (value === "AC") {
+            display.innerText = "0";
+        }
+        else if (value === "DEL") {
+            if (display.innerText === "" || display.innerText === "0" || display.innerText.length == 1) {
+                display.innerText = "0";
+            } else {
+                display.innerText = display.innerText.slice(0, -1);
+            }
         }
 
-        if (value === "AC"){
-            display.innerText="0";
-        }
-        else{
-        display.innerText=display.innerText + value;
+        else if (value==="="){
+            
         }
 
-        
-       
-        
-        
-        
+        else {
+            if (display.innerText === "0") {
+                display.innerText = value;
+            } else {
+                // display.append(value);
+                display.innerText = display.innerText + value;
+            }
+        }
+
     })
 })
